9020d71d5c3077287b4faa221ce26e026807eb26
karabanb <b.karaban@me.com>
2019-03-31 12:36:38 +0200

    dodanie EDA i zmiana nazwy folderu z danymi

--- excercises/020_Modeling.R
+++ excercises/020_Modeling.R
@@ -10,10 +10,32 @@ library(funModeling)
 
 #######  DANE  #####################################################################################
 
-credits <- read.csv('data/german_credit_data.csv')
+credits <- read.csv('dane/german_credit_data.csv')
 
 #######  ESKLPORACYJNA ANALIZA DANYCH  #############################################################
 
+### Wyjaśnienie znaczenia zmiennnych ###
+
+
+# Sprawdź rozmiar ramki danych (ilość obserwacji)
+
+dim(credits)
+
+# sprawdź typy danych w naszej ramce używając funkcji bazowej i funkcji z pakietu dplyr
+
+str(credits)
+glimpse(credits)
+
+# wyświetl podsumowanie ramki, użyj: summary (wybrane statystyki opisowe) i funModeling::df_status
+
+summary(credits)
+df_status(credits)
+
+# czy któraś ze zmiennych 
 # 
+
+
+
+
 
 
