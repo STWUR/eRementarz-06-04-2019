c4645f28c24c5f9d413d55dae82e42d0d3434ed7
karabanb <b.karaban@me.com>
2019-03-31 12:36:38 +0200

    dodanie EDA i zmiana nazwy folderu z danymi

--- excercises/020_Modeling.R
+++ excercises/020_Modeling.R
@@ -14,6 +14,13 @@ credits <- read.csv('data/german_credit_data.csv')
 
 #######  ESKLPORACYJNA ANALIZA DANYCH  #############################################################
 
-# 
+# Sprawdź rozmiar ramki danych (ilość obserwacji)
+dim(credits)
+
+# wyświetl podsumowanie dla ramki (wybrane statystyki opisowe)
+summary(credits)
+
+
+
 
 
