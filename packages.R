77d964703e270dc2b7fef0e2e3ec6636f384ac00
karabanb <b.karaban@me.com>
2019-04-03 13:09:26 +0200

    dodanie pakietu 'MLmetrics' do listy potrzebnych

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
-install.packages(c("tidyverse", "mlr", "rpart", "rpart.plot", "randomForest", "funModeling", "sqldf"), 
+install.packages(c("tidyverse", "mlr", "rpart", "rpart.plot", "randomForest", "funModeling", "sqldf", 'MLmetrics'), 
                  repos = "https://cloud.r-project.org/")
 
 df  <- read.csv('https://raw.githubusercontent.com/STWUR/eRementarz-06-04-2019/master/dane/german_credit_data.csv')
