987d5fdad7ee2e6cff985304c4ef7c241925d44c
Bartosz Pawliczak <bartosz.pawliczak@pwr.edu.pl>
2019-04-05 23:49:06 +0200

    Wersja na prezentację

--- wstep_skrypt.R
+++ wstep_skrypt.R
@@ -1,3 +1,5 @@
+#Skrypt do æwiczeñ - Erementarz - 6.04.18
+
 data <- read.csv("dane/german_credit_data.csv", 
                  encoding = "UTF-8")
 
@@ -9,20 +11,51 @@ data[1:4,1:11]
 head(data)
 sapply(data, class)
 
+data2<-data
+data2$Job <- factor(x = as.character(data$Job), 
+                    labels = 
+                      c('unskilled and non-resident', 
+                        'unskilled and resident',
+                        'skilled','highly skilled'))
+head(data2$Job)
+
+data2$Risk<-as.numeric(x=data$Risk,labels=c(1,2))
+head(data2$Risk)#1-bad, 2-good
+
 # podsumowanie tabeli
 summary(data)
 
 head(is.na(data))
-data<-na.omit(data)
+data2<-na.omit(data)
 
 
 library(sqldf)
-zapytanie1<-"select Age, Sex, Risk from data order by Age desc limit 5" 
+zapytanie1<-"select Age, Sex, Risk from data2 order by Age desc limit 5" 
+sqldf(zapytanie1)
 
 
 zapytanie0<-read.csv.sql("dane/german_credit_data.csv",
                          sql = "select Age, Sex, Risk from file order by Age desc limit 5")
+sqldf(zapytanie0)
+
+zapytanie2 = "select avg(Age) as sredni_wiek from data2 where Sex='male' and Risk=1" 
+sqldf(zapytanie2)
+
+
+zapytanie4 = 'select Sex,Risk, avg(Age) as [Wiek], avg("Credit.amount") as [Kwota kredytu] from data2 group by Sex,Risk'
+sqldf(zapytanie4)
+
+
 
 library(dplyr)
-print(zapytanie0)
-sqldf(zapytanie1)
+select(data2,Age,Sex,Risk)
+
+##Konstrukcja chain
+  data2 %>%
+  select(Age,Sex,Risk) %>%
+  arrange(Age) %>%
+  top_n(5)
+
+data2 %>%
+  filter (Sex=='male', Risk==1) %>%
+  summarise(sredni_wiek=mean(Age)) 
